f302fa6de50acb89682b44cc6c136b5e13f08075
mbenz <mbenz10120112@gmail.com>
2022-07-24 20:10:10 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Box, Grid, TextField, Button, Chip } from "@mui/material";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await axios.get("http://localhost:3000/hash");
+      const pairs = {};
+      data.forEach(
+        (pair) => (pairs[pair.hash] = [...(pairs[pair.hash] || []), pair.content])
+      );
+      setData(pairs);
+    };
+    fetch();
+  }, []);
+
+  const [inputData, setInputData] = useState({ hash: "", content: "" });
+
+  const handleChange = (event) => {
+    setInputData({ ...inputData, [event.target.name]: event.target.value });
+  };
+
+  const handleAddClick = async () => {
+    if (!inputData.hash || !inputData.content) return;
+    const response = await axios.post("http://localhost:3000/hash", {
+      ...inputData,
+    });
+    const newPair = response.data;
+    console.log(data, newPair)
+    const contents = data[newPair.hash] || [];
+    if (!contents.includes(newPair.content)) {
+      contents.push(newPair.content);
+    }
+    setData({ ...data, [newPair.hash]: contents });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box m={5}>
+        <Grid container alignItems="center" gap={5}>
+          <Grid item>
+            <TextField
+              label="Hash"
+              name="hash"
+              value={inputData.hash}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Content"
+              name="content"
+              value={inputData.content}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Button variant="contained" size="large" onClick={handleAddClick}>
+            Add
+          </Button>
+        </Grid>
+        {Object.keys(data).map((hash) => (
+          <Grid container m={3} key={hash} gap={3}>
+            <Chip label={hash}></Chip>
+            <Box display="flex" flexDirection="column">
+              {data[hash].map((content) => (
+                <Chip label={content}></Chip>
+              ))}
+            </Box>
+          </Grid>
+        ))}
+      </Box>
     </div>
   );
 }
